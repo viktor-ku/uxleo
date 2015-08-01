55ec217163779bd54700e41fbf5fda99c2fc6f24
ViktorKonsta <viktor.konsta@gmail.com>
2015-08-02 02:11:11 +0300

    working like in old version project

--- src/js/vendor/vendor.js
+++ src/js/vendor/vendor.js
@@ -2,3 +2,4 @@
 
 //= http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenLite.min.js
 //= http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/ScrollToPlugin.min.js
+//= ../../bower/waypoints/lib/noframework.waypoints.min.js
